30d560808e94141fbd329954d559f45aadfeee39
NathaelB <kirua48000@gmail.com>
2023-03-14 21:10:44 +0100

    :construction: add bouncer, implement method and create validator

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from "App/Models/User";
+import {StoreValidator, UpdateValidator} from "App/Validators/UserValidator";
 
 export default class UsersController {
   public async index ({ bouncer }: HttpContextContract): Promise<User[]> {
@@ -20,18 +21,34 @@ export default class UsersController {
     })
   }
 
-  public async store ({ bouncer }: HttpContextContract) {
+  public async store ({ request, bouncer }: HttpContextContract) {
     await bouncer.with('UserPolicy').authorize('store')
+    const data = await request.validate(StoreValidator)
+
+    return User.create(data)
   }
 
-  public async update ({ bouncer, params }: HttpContextContract) {
+  public async update ({ bouncer, params, request }: HttpContextContract) {
     const user = await User.findOrFail(params.id)
     await bouncer.with('UserPolicy').authorize('update', user)
 
+    const data = await request.validate(UpdateValidator)
+
+    if (data.permissions) {
+      await user.related('permissions').sync(data.permissions)
+    }
+
+    if (data.roles) {
+      await user.related('roles').sync(data.roles)
+    }
+
+    return user.merge(data).save()
   }
 
   public async destroy ({ bouncer, params }: HttpContextContract) {
     const user = await User.findOrFail(params.id)
     await bouncer.with('UserPolicy').authorize('delete', user)
+
+    await user.delete()
   }
 }
